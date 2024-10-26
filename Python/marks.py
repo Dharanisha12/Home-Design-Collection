703a98390ebb4b39b1ba0cbba768bc52d75e93f3
Dharanisha K P <dharanishakp@gmail.com>
2024-10-26 20:57:50 +0530

    Add files via upload

---
+++ Python/marks.py
@@ -0,0 +1,12 @@
+print("student Status")
+i=int(input("Enter the marks:"))
+if(i>=90):
+    print("A Grade")
+elif(i>=80):
+    print("B Grade")
+elif(i>=60):
+    print("C Grade")
+elif(i>=50):
+    print("D Grade")
+else:
+    print("Try Again")
